cfcc4a7340f14c19ce849a0305216863d69b45d6
Cintya Agusti Anindita <cintyaagusti99@mail.ugm.ac.id>
2020-05-13 10:51:41 +0700

    Updates

---
+++ precache-manifest.c29205c22951baa6f27740054568ee47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a658c6e3ef9807658a4688d8fd421f",
+    "url": "/Responsi-Web3/index.html"
+  },
+  {
+    "revision": "6151c8bb9e858c2637f4",
+    "url": "/Responsi-Web3/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "806ae7985cfb695ba42a",
+    "url": "/Responsi-Web3/static/css/main.2236a508.chunk.css"
+  },
+  {
+    "revision": "6151c8bb9e858c2637f4",
+    "url": "/Responsi-Web3/static/js/2.8aa60295.chunk.js"
+  },
+  {
+    "revision": "88a28bf9811004a7250f8b652924c27d",
+    "url": "/Responsi-Web3/static/js/2.8aa60295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "806ae7985cfb695ba42a",
+    "url": "/Responsi-Web3/static/js/main.53c4d46d.chunk.js"
+  },
+  {
+    "revision": "0c3edafb4e7f0c28dda8",
+    "url": "/Responsi-Web3/static/js/runtime-main.4a4a45fe.js"
+  }
+]);
